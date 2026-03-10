564e9c63845fe5db0e55ae874fdd52148b58f63d
kuzey5921-sudo <kuzey5921@gmail.com>
2026-03-10 22:13:43 +0300

    bitti

---
+++ src/test/java/Person.java
@@ -0,0 +1,47 @@
+
+
+public class Person {
+
+    // Instance variables
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    // Eklediğimiz 3 değişken
+    private String gender;
+    private String city;
+    private String job;
+
+    // Constructor 1
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Constructor 2 (Overloading + Chaining)
+    public Person(String firstName, String lastName, int age, String gender, String city, String job) {
+        this(firstName, lastName, age); // constructor chaining
+        this.gender = gender;
+        this.city = city;
+        this.job = job;
+    }
+
+    // Methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+
+}
